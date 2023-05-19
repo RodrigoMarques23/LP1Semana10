ea66629b6af4043b7a39e0abcb8cf3abeb53b7d0
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-05-19 02:00:09 +0100

    Ex03 Solved

---
+++ StringCollections/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace StringCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "baba";
+            string b = "baba";
+            string c = "mundo";
+            string d = "alien";
+            string e = "extra";
+
+            List<string> list = new List<string> { a, b, c, d, e };
+            Stack<string> stack = new Stack<string>();
+            Queue<string> queue = new Queue<string>();
+            foreach(string s in list)
+            {
+                stack.Push(s);
+                queue.Enqueue(s);
+            }
+            HashSet<string> hashSet = new HashSet<string> { a, b, c, d, e };
+
+            Console.WriteLine("List:");
+            foreach (string s in list)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("stack:");
+            foreach (string s in stack)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Queue:");
+            foreach (string s in queue)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("hashSet:");
+            foreach (string s in hashSet)
+            {
+                Console.WriteLine(s);
+            }
+
+
+        }
+    }
+}
